c600995fcc5fd051d9daab149c0e969b7e0d2b9e
Alistair Knox <knoxy_67@yahoo.com.au>
2013-05-15 23:41:44 +1000

    working on login
    just started

--- ganttly/urls.py
+++ ganttly/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, url
 from ganttly import views
+from django.contrib import admin
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^projects/$', views.project_list, name='project_list'),
-    url(r'^projects/(?P<project_id>\d+)/$', views.project, name='project'),
-    url(r'^projects/(?P<project_id>\d+)/tasks/$', views.task_list, name='task_list'),
-    url(r'^projects/(?P<project_id>\d+)/tasks/(?P<task_id>\d+)/$', views.task, name='task'),
+    	url(r'^projects/(?P<project_id>\d+)/$', views.project, name='project'),
+    	url(r'^projects/(?P<project_id>\d+)/tasks/$', views.task_list, name='task_list'),
+    	url(r'^projects/(?P<project_id>\d+)/tasks/(?P<task_id>\d+)/$', views.task, name='task'),
+	url(r'^login/','django.contrib.auth.views.login',	
+		{'template_name':'login.html'}),
 )
