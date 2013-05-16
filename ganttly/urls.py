9d642d05419edcabeaf800cecd0ca5e57c3ecf89
Brendan <mbrendan@student.unimelb.edu.au>
2013-05-16 23:27:32 +1000

    merging

--- ganttly/urls.py
+++ ganttly/urls.py
@@ -3,17 +3,11 @@ from ganttly import views
 from django.contrib import admin
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
+	#url(r'^$', views.index, name='index'),
+    url(r'^$','django.contrib.auth.views.login',
+        {'template_name':'login.html'}),
 	url(r'^projects/$', views.project_list, name='project_list'),
-<<<<<<< HEAD
     url(r'^projects/(?P<project_id>\d+)/$', views.project, name='project'),
     #url(r'^projects/(?P<project_id>\d+)/tasks/$', views.task_list, name='task_list'),
     url(r'^projects/(?P<project_id>\d+)/(?P<task_id>\d+)/$', views.task, name='task'),
-=======
-    	url(r'^projects/(?P<project_id>\d+)/$', views.project, name='project'),
-    	url(r'^projects/(?P<project_id>\d+)/tasks/$', views.task_list, name='task_list'),
-    	url(r'^projects/(?P<project_id>\d+)/tasks/(?P<task_id>\d+)/$', views.task, name='task'),
-	url(r'^login/','django.contrib.auth.views.login',	
-		{'template_name':'login.html'}),
->>>>>>> origin/Al
 )
